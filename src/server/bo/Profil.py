68087db915e89a08504d201dd0f058e91d6d0254
leonard baranski <lb151@hdm-stuttgart.de>
2021-05-05 15:38:00 +0200

    Grundbausteine des Klasse Profil gelegt. Vererbung wird auf BO gewartet.

---
+++ src/server/bo/Profil.py
@@ -0,0 +1,12 @@
+# from server.bo import BusinessObject as bo
+# from server.bo import Lernvorliebe
+# from server.bo import Person
+
+
+class Profil("""bo.BusinessObject"""):
+
+    def __init__(self):
+        """BO erben"""
+
+    def erstelle_Profil(self, Lernvorliebe, Person):
+        pass
