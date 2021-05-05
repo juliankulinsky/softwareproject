b1b7dcf6cb20d480a7e7bc6d706b8ba573b4d428
leonard baranski <lb151@hdm-stuttgart.de>
2021-05-05 21:53:25 +0200

    Grundbausteine der Klasse Vorschlag gelegt

---
+++ src/server/bo/Vorschlag.py
@@ -0,0 +1,26 @@
+# from server.bo import BusinessObject as bo
+from server.bo import Profil
+
+
+class Vorschlag("""bo.BusinessObject"""):
+    """Realisieren der Klasse Vorschlag, welche den Algorithmus zum Matches generien übernimmt"""
+
+    def __init__(self):
+        super().__init__()
+        self._matches = []
+        self._matches_gruppen = []
+        self.profile = Profil
+
+    def zeige_matches(self):
+        """Ausgeben der vorhandenen Matches für ein Profil"""
+        return self._matches
+
+    def zeige_matches_gruppen(self):
+        """Ausgeben der vorgeschlagenen Lerngruppen"""
+        return self._matches_gruppen
+
+    def generiere_matches(self, profil=Profil):
+        """Algo verwirklichen"""
+        self.profile = profil
+        # Wie realisieren?
+
