1c8de83e0ae666132bc7efb6462a9723056d453b
leonard baranski <lb151@hdm-stuttgart.de>
2021-05-10 11:39:00 +0200

    Anfang LernvorliebeMapper.py

---
+++ src/server/db/LernvorliebeMapper.py
@@ -0,0 +1,11 @@
+from server.bo import Lernvorliebe
+from server.db import Mapper
+
+
+class LernvorliebenMapper(Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+        pass
