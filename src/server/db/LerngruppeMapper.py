29e7d75e961ed780224070928ff39b8b42cbf3ac
maximchristmann <mc@hdm-stuttgart.de>
2021-05-12 13:07:24 +0200

    fix: Testing und Syntax

--- src/server/db/LerngruppeMapper.py
+++ src/server/db/LerngruppeMapper.py
@@ -1,25 +1,30 @@
 from server.bo.Lerngruppe import Lerngruppe
 from server.db.Mapper import Mapper
 
+"""Mapper-Klasse, welche Lerngruppe-Objekte in eine relationale Datenbank (DB),
+    und umgekehrt von einer DB auf ein Objekt, abbilden kann.
+    Dazu werden die folgenden Methoden zur Verfügung gestellt,
+    mit denen Objekte gesucht, erzeugt, modifiziert und gelöscht werden können."""
+
 
 class LerngruppeMapper(Mapper):
-    """Mapper-Klasse, welche Lerngruppe-Objekte in eine relationale Datenbank (DB),
-        und umgekehrt von einer DB auf ein Objekt, abbilden kann.
-        Dazu werden die folgenden Methoden zur Verfügung gestellt,
-        mit denen Objekte gesucht, erzeugt, modifiziert und gelöscht werden können."""
-
     def __init__(self):
         super().__init__()
 
     def find_all(self):
-        """Auslesen aller Lerngruppe-Objekte."""
+        """Extrahieren aller Lerngruppe-Objekte."""
 
         result = []
         cursor = self._cnx.cursor()
-        cursor.execute("SELECT * from lerngruppen")
+        cursor.execute("SELECT * from lerngruppen")  # SQL Statement
         tuples = cursor.fetchall()
 
-        for (id, erstellungszeitpunkt, gruppenname, profil_id, konversation_id) in tuples:
+        for (id,
+            erstellungszeitpunkt,
+            gruppenname,
+            profil_id,
+            konversation_id
+        ) in tuples:
             lerngruppe = Lerngruppe()
             lerngruppe.set_id(id)
             lerngruppe.set_erstellungszeitpunkt(erstellungszeitpunkt)
@@ -31,16 +36,15 @@ class LerngruppeMapper(Mapper):
         self._cnx.commit()
         cursor.close()
 
-        return result   # Rückgabe der Sammlung aller Lerngruppe-Objekte
+        return result  # Rückgabe der Sammlung aller Lerngruppe-Objekte
 
     def find_by_key(self, key):
         """Suchen einer Lerngruppe mit vorgegebener ID."""
 
         result = None
         cursor = self._cnx.cursor()
-        command = "SELECT id, erstellungszeitpunkt, gruppenname, profil_id, konversation_id FROM " \
-                  "lerngruppen WHERE id={}"\
-            .format(key)
+        command = ("SELECT id, erstellungszeitpunkt, gruppenname, profil_id, konversation_id FROM "
+                    "lerngruppen WHERE id={}".format(key))
         cursor.execute(command)
         tuples = cursor.fetchall()
 
@@ -54,15 +58,13 @@ class LerngruppeMapper(Mapper):
             lerngruppe.set_konversation_id(konversation_id)
             result = lerngruppe
 
-        except IndexError:
-            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
-                        keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+        except IndexError:  # Wenn eine leere Sequenz zurückgegeben wird (tuples).
             result = None
 
         self._cnx.commit()
         cursor.close()
 
-        return result   # Der Rückgabe der ID entsprechendes Lerngruppe-Objekt (None bei fehlender DB-Tupel)
+        return result  # Der Rückgabe der ID entsprechendes Lerngruppe-Objekt (None bei fehlender DB-Tupel)
 
     def insert(self, lerngruppe):
         """Einfügen eines Lerngruppe-Objekts in die Datenbank.
@@ -72,13 +74,16 @@ class LerngruppeMapper(Mapper):
         cursor.execute("SELECT MAX(id) AS maxid FROM lerngruppen")
         tuples = cursor.fetchall()
 
-        for (maxid) in tuples:
+        for maxid in tuples:
             lerngruppe.set_id(maxid[0] + 1)
 
-        command = "INSERT INTO lerngruppen (id, erstellungszeitpunkt, gruppenname, profil_id, konversation_id) " \
-                  "VALUES (%s,%s,%s,%s,%s)"
-        data = (lerngruppe.get_id(), lerngruppe.get_erstellungszeitpunkt(), lerngruppe.get_gruppenname(),
-                lerngruppe.get_profil_id(), lerngruppe.get_konversation_id())
+        command = ("INSERT INTO lerngruppen (id, erstellungszeitpunkt, gruppenname, profil_id, konversation_id) "
+            "VALUES (%s,%s,%s,%s,%s)")
+        data = (lerngruppe.get_id(),
+            lerngruppe.get_erstellungszeitpunkt(),
+            lerngruppe.get_gruppenname(),
+            lerngruppe.get_profil_id(),
+            lerngruppe.get_konversation_id())
         cursor.execute(command, data)
 
         """Optional:
@@ -91,24 +96,34 @@ class LerngruppeMapper(Mapper):
         self._cnx.commit()
         cursor.close()
 
-        return lerngruppe   # Rückgabe des bereits übergebenen Objektes, ggf. mit korrigierter ID
+        return lerngruppe  # Rückgabe des bereits übergebenen Objektes, ggf. mit korrigierter ID
 
-    def update(self, lerngruppen):
+    def update(self, lerngruppe):
         """Aktualisieren eines Objekts in der Datenbank anhand seiner ID."""
 
         cursor = self._cnx.cursor()
 
-        command = "DELETE FROM lerngruppen WHERE id={}".format(lerngruppen.get_id())
-        cursor.execute(command)
+        command = (
+            "UPDATE personen "
+            + "SET erstellungszeitpunkt=%s, get_gruppenname=%s, profil_id=%s, konversation_id=%s"
+            + "WHERE id=%s"
+        )
+        data = (
+            lerngruppe.get_erstellungszeitpunkt(),
+            lerngruppe.get_gruppenname(),
+            lerngruppe.get_profil_id(),
+            lerngruppe.get_konversation_id(),
+        )
+        cursor.execute(command, data)
 
         self._cnx.commit()
         cursor.close()
 
-    def delete(self, lerngruppen):
+    def delete(self, lerngruppe):
         """Löschen eines Lerngruppe-Objekts aus der Datenbank."""
         cursor = self._cnx.cursor()
 
-        command = "DELETE FROM lerngruppen WHERE id={}".format(lerngruppen.get_id())
+        command = "DELETE FROM lerngruppen WHERE id={}".format(lerngruppe.get_id())
         cursor.execute(command)
 
         self._cnx.commit()
@@ -116,27 +131,38 @@ class LerngruppeMapper(Mapper):
 
 
 """Testing für Klasse"""
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     with LerngruppeMapper() as mapper:
 
         test = Lerngruppe()
         test.set_gruppenname("Funky Python Kurs")
-        test.set_profil_id(6)
-        test.set_konversation_id(6)
+        test.set_profil_id(7)
+        test.set_konversation_id(7)
 
+
+        #   Methode insert
+        print("Test der Methode: .insert")
         mapper.insert(test)
 
-        # Alle Daten aus der Tabelle ausgeben
-        print("Find all")
+
+        #  Methode find_all
+        print("Test der Methode: find_all")
         result = mapper.find_all()
         for r in result:
             print(r.__dict__)
 
+        # Methode find_by_key
+        print("Test der Methode: .find_by_key")
+        lostkey = mapper.find_by_key(1)
+        print(lostkey.__dict__)
 
-        """# Löschen
-        print("Delete")
-        mapper.delete(test)
+        # Methode delete
+        print("Test der Methode: .delete")
+        weg = mapper.find_by_key(3)
+        mapper.delete(weg)
 
+        # Nochmal find_all
+        print("Erneut Methode: find_all")
         result = mapper.find_all()
         for r in result:
-            print(r.__dict__)"""
+            print(r.__dict__)
