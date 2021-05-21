6c0d43a3e298e16fa7bd96f8ae0c3ca38a9441b4
maximchristmann <mc@hdm-stuttgart.de>
2021-05-21 11:58:51 +0200

    fix: Finalisierung der LerngruppenBO.js

--- frontend/src/api/LerngruppeBO.js
+++ frontend/src/api/LerngruppeBO.js
@@ -4,12 +4,41 @@ import BusinessObject from "./BusinessObject";
 export default class LerngruppeBO extends BusinessObject{
     /** Diese Klasse erstellt ein BusinessObject einer Lerngruppe. */
 
-    constructor() {
+    constructor(aGruppenname, aProfilId, aKonversationId) {
         super();
         this.gruppenname = aGruppenname;
         this.profil_id = aProfilId;
         this.konversation_id = aKonversationId;
     }
-}
 
-// Brauchen wir hier überhaupt Getter & Setter?
+    // Nun die jeweiligen Getter & Setter der Lerngruppe:
+    /** Setzt den Gruppennamen. */
+    setGruppenname(aGruppenname) {
+    this.gruppenname = aGruppenname;
+    }
+
+    /** Gibt den Gruppennamen aus. */
+    getGruppenname() {
+        return this.gruppenname;
+    }
+
+    /** Setzt die Profil ID. */
+    setProfilId(aProfilId) {
+    this.profil_id = aProfilId;
+    }
+
+    /** Gibt die Profil ID aus. */
+    getProfilId() {
+        return this.profil_id;
+    }
+
+    /** Setzt die Konversation ID. */
+    setKonversationId(aKonversationId) {
+    this.konversation_id = aKonversationId;
+    }
+
+    /** Gibt den Konversation ID aus. */
+    getKonversationId() {
+        return this.konversation_id;
+    }
+}
