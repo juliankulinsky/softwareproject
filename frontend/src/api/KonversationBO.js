a2ac015e0b39f92a6776bf121de66a90d5455cba
maximchristmann <mc067@hdm-stuttgart.de>
2021-05-24 11:57:12 +0200

    fix: Testing und JSON Methode für KonversationBO.js

--- frontend/src/api/KonversationBO.js
+++ frontend/src/api/KonversationBO.js
@@ -19,4 +19,39 @@ export default class KonversationBO extends BusinessObject {
     getIstGruppenchat() {
         return this.gruppenchat;
     }
+
+    // Debugging Methoden
+    /** Gibt die Entscheidung als String aus. */
+    toString() {
+    let result = '';
+
+    for (var prop in this) {
+      result += prop + ': ' + this[prop] + ' ';
+        }
+
+    return result;
+    }
+
+    // JSON Methoden
+    /** Gibt Array von PartnerVorschlagBO einer gegebenen JSON-Struktur zurück. */
+    static fromJSON(konversation) {
+        let result = [];
+
+        if (Array.isArray(konversation)) {
+            konversation.forEach((t) => {
+                Object.setPrototypeOf(t, KonversationBO.prototype);
+                result.push(t);
+            })
+        }
+        else {
+            // Sollte es sich um ein singuläres Objekt handeln.
+            let t = konversation;
+
+            Object.setPrototypeOf(t, KonversationBO.prototype);
+            result.push(t);
+        }
+
+        return result;
+    }
 }
+
