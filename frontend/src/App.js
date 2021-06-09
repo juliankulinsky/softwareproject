71ea15aae8b5cd32aed0e54a6ff9261d57377d76
AndreasScheumann <as380@hdm-stuttgart.de>
2021-06-09 13:06:34 +0200

    feat: grundlegenden frontend Aufbau inkl. Firebase SignIn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,19 @@ import { Container, ThemeProvider, CssBaseline } from '@material-ui/core';
 import PersonenList from './components/PersonenList';
 import PersonListEntry from "./components/PersonListEntry";
 import LerngruppenList from "./components/LerngruppenList";
-/*import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import Header from './components/layout/Header';
-import TransactionList from './components/TransactionList';
+/*import TransactionList from './components/TransactionList';
 import About from './components/pages/About';
 import AllAccountList from './components/AllAccountList';
 import Theme from './Theme';
+
+ */
 import SignIn from './components/pages/SignIn';
 import LoadingProgress from './components/dialogs/LoadingProgress';
 import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
 import firebaseConfig from './firebaseconfig';
-*/
 import TestList from './components/TestList';
 import AllProfile from './components/AllProfile';
 import ProfilEntry from "./components/ProfilEntry";
@@ -59,6 +60,76 @@ class App extends React.Component {
         return {appError: error};
     }
 
+    /** Handles firebase users logged in state changes  */
+	handleAuthStateChange = user => {
+		if (user) {
+			console.log("im HANDLEAUTHSTATECHANGE WTF")
+			this.setState({
+				authLoading: true
+			});
+			// The user is signed in
+			user.getIdToken().then(token => {
+				// Add the token to the browser's cookies. The server will then be
+				// able to verify the token against the API.
+				// SECURITY NOTE: As cookies can easily be modified, only put the
+				// token (which is verified server-side) in a cookie; do not add other
+				// user information.
+				document.cookie = `token=${token};path=/`;
+				console.log("Vorm setstate mit User: " + user);
+				console.log(user)
+				console.log("Token:")
+				console.log(token)
+
+				// Set the user not before the token arrived
+				this.setState({
+					currentUser: user,
+					authError: null,
+					authLoading: false
+				});
+			}).catch(e => {
+				this.setState({
+					authError: e,
+					authLoading: false
+				});
+			});
+		} else {
+			// User has logged out, so clear the id token
+			document.cookie = 'token=;path=/';
+
+			// Set the logged out user to null
+			this.setState({
+				currentUser: null,
+				authLoading: false
+			});
+		}
+	}
+
+	/**
+   * Handles the sign in request of the SignIn component uses the firebase.auth() component to sign in.
+	 * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+	 * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+	 */
+	handleSignIn = () => {
+		this.setState({
+			authLoading: true
+		});
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	}
+
+	/**
+	 * Lifecycle method, which is called when the component gets inserted into the browsers DOM.
+	 * Initializes the firebase SDK.
+	 *
+	 * @see See Googles [firebase init process](https://firebase.google.com/docs/web/setup)
+	 */
+	componentDidMount() {
+		firebase.initializeApp(firebaseConfig);
+		firebase.auth().languageCode = 'en';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+	}
+
+
     //Automatisches Weiterleiten <Redirect from='/' to='/studoo/lerngruppen'/>
     /** Renders the whole app */
     render() {
@@ -67,27 +138,39 @@ class App extends React.Component {
         return (
                 <Router basename={process.env.PUBLIC_URL}>
                     <Container maxWidth='md'>
-                        <Route path='/studoo/lerngruppen'>
-                            <TestList />
-                        </Route>
-                        <Route path='/studoo/personen'>
-                            <PersonenList />
-                        </Route>
-                        <Route path='/studoo/profil'>
-                            <AllProfile />
-                        </Route>
-                        <Route path='/studoo/gruppenvorschlaege'>
-                            <AllGruppenvorschlaege />
-                        </Route>
-                        <Route path='/studoo/partnervorschlaege'>
-                            <AllPartnervorschlaege />
-                        </Route>
-                        <Route path='/studoo/lernvorlieben'>
-                            <LernvorliebenList />
-                        </Route>
-                        <Route path='/studoo/nachrichten'>
-                            <AllNachrichten />
-                        </Route>
+						<Header user={currentUser} />
+						{
+							currentUser ?
+								<>
+									<Route path='/studoo/lerngruppen'>
+										<TestList />
+									</Route>
+									<Route path='/studoo/personen'>
+										<PersonenList />
+									</Route>
+									<Route path='/studoo/profil'>
+										<AllProfile />
+									</Route>
+									<Route path='/studoo/gruppenvorschlaege'>
+										<AllGruppenvorschlaege />
+									</Route>
+									<Route path='/studoo/partnervorschlaege'>
+										<AllPartnervorschlaege />
+									</Route>
+									<Route path='/studoo/lernvorlieben'>
+										<LernvorliebenList />
+									</Route>
+									<Route path='/studoo/nachrichten'>
+										<AllNachrichten />
+									</Route>
+								</>
+								:
+								<>
+									<Redirect to='/index.html' />
+									<SignIn onSignIn={this.handleSignIn} />
+								</>
+						}
+
                     </Container>
                 </Router>
         );
---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,12 @@
+/** The firebase config structure for the Python Bank Project as provided by the firebase admin website */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAksPDsDpZdTOWKUQQ26IiEWebl2uxeF1E",
+  authDomain: "bankprojekt-d8417.firebaseapp.com",
+  projectId: "bankprojekt-d8417",
+  storageBucket: "bankprojekt-d8417.appspot.com",
+  messagingSenderId: "460489310530",
+  appId: "1:460489310530:web:12dd0b119722d8581348db"
+};
+
+export default firebaseConfig;
