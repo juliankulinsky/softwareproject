507bf1065a82f10c740ad253623cdd16cfaf62e2
kulinsky <juliankulinsky@web.de>
2021-06-11 12:48:19 +0200

    fix: Header-Breite auf full-screen

--- frontend/src/components/header/HeaderComplete.js
+++ frontend/src/components/header/HeaderComplete.js
@@ -20,7 +20,7 @@ class HeaderComplete extends Component {
         return (
             <>
                 <div>
-                    <AppBar position="sticky" style={theme.root}>
+                    <AppBar style={theme.root}>
                         <Toolbar style={theme.toolbar}>
                             <div style={theme.imagediv}>
                                 <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="Logo" style={theme.image}/>
