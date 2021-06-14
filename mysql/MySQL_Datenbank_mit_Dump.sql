dedbf1a69482187344a53e04bbf83c800fa37cb3
AndreasScheumann <as380@hdm-stuttgart.de>
2021-06-14 11:57:39 +0200

    feat: Datenbank mit mehr Testdaten für eine neuangemeldete Person

--- mysql/MySQL_Datenbank_mit_Dump.sql
+++ mysql/MySQL_Datenbank_mit_Dump.sql
@@ -33,7 +33,7 @@ LOCK TABLES `personen` WRITE;
 INSERT INTO `personen`
 VALUES (1,'2021-04-02 18:21:26','Richard Müller', 'richi.mülli@gmail.com', 'useridlululul', 28,'Stuttgart','WI7',2,1),
        (2,'2021-04-21 15:36:01','Otto Roller', 'derotto@gmail.com', '1234otto4321',39,'Stuttgart','IM7',3,2),
-       (3,'2021-05-08 10:36:01','Lisa Ritter', 'l.ritter@gmail.com', 'l1i2s3a4', 22,'Filderstadt','OM',1,3);
+       (3,'2021-05-08 10:36:01','Lisa Ritter', 'l.ritter@gmail.com', 'l1i2s3a4', 22,'Filderstadt','OM',1,7);
 UNLOCK TABLES;
 
 --
@@ -55,7 +55,13 @@ CREATE TABLE `profile` (
 LOCK TABLES `profile` WRITE;
 INSERT INTO `profile`
 VALUES (1,'2021-03-02 18:21:26',1),
-       (2,'2021-04-26 15:36:01',2);
+       (2,'2021-04-26 15:36:01',2),
+       (3,'2021-04-26 15:37:01',3),
+       (4,'2021-04-26 15:39:01',4),
+       (5,'2021-04-26 15:39:01',5),
+       (6,'2021-04-26 15:39:01',6),
+       (7,'2021-04-26 15:39:01',7),
+       (8,'2021-04-26 15:39:01',8);
 UNLOCK TABLES;
 
 --
@@ -82,7 +88,13 @@ CREATE TABLE `lernvorlieben` (
 LOCK TABLES `lernvorlieben` WRITE;
 INSERT INTO `lernvorlieben`
 VALUES (1,'2021-03-02 18:21:26',4,3,1,2,'Python','Apps'),
-       (2,'2021-04-26 15:36:01',2,4,3,2,'React','Websites');
+       (2,'2021-04-26 15:36:01',2,4,3,2,'React','Websites'),
+       (3,'2021-04-26 15:37:01',4,5,3,2,'Java','Python'),
+       (4,'2021-04-26 15:39:01',2,4,3,2,'Mathe','Websites'),
+       (5,'2021-04-26 15:39:01',4,1,1,1,'React','Apps'),
+       (6,'2021-04-26 15:39:01',2,1,3,1,'Nichts','Python'),
+       (7,'2021-04-26 15:39:01',4,1,2,3,'Design','Java'),
+       (8,'2021-04-26 15:39:01',4,5,5,5,'Apps','Data Science');
 UNLOCK TABLES;
 
 --
@@ -106,9 +118,22 @@ CREATE TABLE `nachrichten` (
 LOCK TABLES `nachrichten` WRITE;
 INSERT INTO `nachrichten`
 VALUES (1,'2021-05-01 15:36:01','Hi, wie gehts dir',1,2),
-       (2,'2021-05-01 16:02:45','Gut dir?',2,1),
+       (2,'2021-05-01 16:02:45','Gut dir?',2,2),
        (3,'2021-05-02 09:50:59','Mir auch danke',1,2),
-       (4,'2021-05-02 12:36:12','Kannst du Python?',1,2);
+       (4,'2021-05-02 12:36:12','Hi Kannst du Python?',1,1),
+       (5,'2021-05-02 12:36:12','Leider garnicht',3,1),
+       (6,'2021-06-10 15:30:56','Ich könnte vielleicht helfen',4,1),
+       (7,'2021-06-11 12:42:12','Das wäre echt mega nett',1,1),
+       (8,'2021-06-11 19:36:12','Wollen wir uns mal treffen?',2,2),
+       (9,'2021-06-13 12:36:12','Oha Ehrenmann, wann hast du Zeit?',3,1),
+       (10,'2021-06-13 13:36:12','Oh cool ich hatte noch nie ein Match',2,3),
+       (11,'2021-06-13 13:39:12','Ich bin Otto, wie heißt du?',2,3),
+       (12,'2021-06-13 13:39:36','Hey Lisa, lass uns selbstständig machen!',2,6),
+       (13,'2021-06-13 14:39:36','Jaaaa, richtig Lust! Wo fangen wir an?',3,6),
+       (14,'2021-06-13 16:39:36','Mit was könnten wir denn reich werden?',2,6),
+       (15,'2021-06-13 16:56:36','Oh, hi, jetzt haben wir einen Dritten im Bunde',2,6),
+       (16,'2021-06-13 16:56:36','Was meinst du?',2,6)
+       ;
 UNLOCK TABLES;
 
 --
@@ -129,8 +154,12 @@ CREATE TABLE `konversationen` (
 
 LOCK TABLES `konversationen` WRITE;
 INSERT INTO `konversationen`
-VALUES (1,'2021-03-02 18:21:26',FALSE),
-       (2,'2021-04-26 15:36:01',TRUE);
+VALUES (1,'2021-03-02 18:21:26',TRUE),
+       (2,'2021-04-26 15:36:01',TRUE),
+       (3,'2021-05-29 20:36:01',FALSE),
+       (4,'2021-06-02 15:36:01',TRUE),
+       (5,'2021-06-02 15:36:01',TRUE),
+       (6,'2021-06-06 15:36:01',TRUE);
 UNLOCK TABLES;
 
 --
@@ -152,8 +181,18 @@ CREATE TABLE `chat_teilnahmen` (
 
 LOCK TABLES `chat_teilnahmen` WRITE;
 INSERT INTO `chat_teilnahmen`
-VALUES (1,'2021-03-02 18:21:26',2,1),
-       (2,'2021-04-26 15:36:01',3,1);
+VALUES (1,'2021-03-02 18:21:26',1,2),
+       (2,'2021-04-26 15:36:01',2,2),
+       (3,'2021-04-26 15:36:01',1,1),
+       (4,'2021-04-26 15:36:01',3,1),
+       (5,'2021-04-26 15:36:01',3,3),
+       (6,'2021-04-26 15:36:01',4,1),
+       (7,'2021-04-26 15:36:01',4,2),
+       (8,'2021-04-26 15:36:01',4,3),
+       (9,'2021-04-26 15:36:01',4,4),
+       (10,'2021-04-26 15:36:01',2,6),
+       (11,'2021-04-26 15:36:01',3,6)
+       ;
 UNLOCK TABLES;
 
 --
@@ -179,7 +218,10 @@ CREATE TABLE `partner_vorschlaege` (
 LOCK TABLES `partner_vorschlaege` WRITE;
 INSERT INTO `partner_vorschlaege`
 VALUES (1,'2021-03-02 18:21:26',1,2,67.8,TRUE,TRUE),
-       (2,'2021-04-26 15:36:01',1,3,67.8,TRUE,NULL);
+       (2,'2021-04-26 15:36:01',1,3,67.8,TRUE,NULL),
+       (3,'2021-04-26 15:36:01',4,1,81.2,NULL,NULL),
+       (4,'2021-04-26 15:36:01',4,2,90.3,TRUE,NULL),
+       (5,'2021-04-26 15:36:01',4,3,57.8,NULL,NULL);
 UNLOCK TABLES;
 
 --
@@ -202,8 +244,11 @@ CREATE TABLE `lerngruppen` (
 
 LOCK TABLES `lerngruppen` WRITE;
 INSERT INTO `lerngruppen`
-VALUES (1,'2021-03-02 18:21:26','Python Profis',4,2),
-       (2,'2021-04-26 15:36:01','Java Genies',5,3);
+VALUES (1,'2021-03-02 18:21:26','Road-To-Python-Profis',3,2),
+       (2,'2021-04-26 15:36:01','Ersti-Treff',4,1),
+       (3,'2021-04-26 15:36:01','Shy-Guys',5,4),
+       (4,'2021-04-26 15:36:01','DIE NEUEN',6,5),
+       (5,'2021-04-26 15:36:01','Die Unternehmer',8,6);
 UNLOCK TABLES;
 
 --
@@ -226,8 +271,15 @@ CREATE TABLE `gruppen_teilnahmen` (
 
 LOCK TABLES `gruppen_teilnahmen` WRITE;
 INSERT INTO `gruppen_teilnahmen`
-VALUES (1,'2021-03-02 18:21:26',1,2,FALSE),
-       (2,'2021-04-26 15:36:01',3,2,FALSE);
+VALUES (1,'2021-03-02 18:21:26',1,1,FALSE),
+       (2,'2021-04-26 15:36:01',2,1,FALSE),
+       (3,'2021-04-26 15:36:01',4,1,FALSE),
+       (4,'2021-04-26 15:36:01',1,2,FALSE),
+       (5,'2021-04-26 15:36:01',3,2,FALSE),
+       (6,'2021-04-26 15:36:01',4,2,FALSE),
+       (7,'2021-04-26 15:36:01',4,3,TRUE),
+       (8,'2021-04-26 15:36:01',2,5,TRUE),
+       (9,'2021-04-26 15:36:01',3,5,TRUE);
 UNLOCK TABLES;
 
 --
@@ -253,5 +305,10 @@ CREATE TABLE `gruppen_vorschlaege` (
 LOCK TABLES `gruppen_vorschlaege` WRITE;
 INSERT INTO `gruppen_vorschlaege`
 VALUES (1,'2021-03-02 18:21:26',1,1,70.5,TRUE,NULL),
-       (2,'2021-04-26 15:36:01',3,2,66.8,FALSE,NULL);
+       (2,'2021-04-26 15:36:01',3,2,66.8,FALSE,NULL),
+       (3,'2021-04-26 15:36:01',4,4,92.8,NULL,NULL),
+       (4,'2021-04-26 15:36:01',4,5,98.2,NULL,NULL),
+       (5,'2021-04-26 15:36:01',4,2,98.2,TRUE,TRUE),
+       (6,'2021-04-26 15:36:01',4,1,98.2,TRUE,TRUE),
+       (7,'2021-04-26 15:36:01',4,3,98.2,TRUE,TRUE);
 UNLOCK TABLES;
