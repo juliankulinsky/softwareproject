d01ccae4922034e63cdc706d42b5e7d315b91c6b
leonard baranski <lb151@hdm-stuttgart.de>
2021-06-17 17:31:21 +0200

    Algorithmus.py WIP

--- src/server/Algorithmus.py
+++ src/server/Algorithmus.py
@@ -1,6 +1,5 @@
 from server.Admin import Admin
 from server.db.PartnerVorschlagMapper import PartnerVorschlagMapper
-from server.db.Mapper import Mapper
 from server.bo.PartnerVorschlag import PartnerVorschlag
 
 
@@ -9,34 +8,7 @@ class Algorithmus:
     def __init__(self):
         pass
 
-    """def getalllernvorlieben(self):
-        adm = Admin()
-        lety = []
-        fre = []
-        ext = []
-        remprae = []
-        vor = []
-        lein = []
-        for step in range(len(adm.get_all_lernvorlieben())):
-            lern = adm.get_lernvorliebe_by_id(step+1)
-            lety.append(lern.get_lerntyp())
-            fre.append(lern.get_frequenz())
-            ext.append(lern.get_extrovertiertheit())
-            remprae.append(lern.get_remote_praesenz())
-            vor.append(lern.get_vorkenntnisse())
-            lein.append(lern.get_lerninteressen())
-            step += 1
-        for x in range(len(adm.get_all_lernvorlieben())):
-            score = lety[x] + fre[x] + ext[x] + remprae[x]
-            id = x+1
-            with PartnerVorschlagMapper() as mapper:
-                result = mapper.find_by_key(id)
-                result.set_aehnlichkeit(score)
-                mapper.update(result)
-        return result.get_aehnlichkeit()"""
-
     def match(self, partnervorschlag: PartnerVorschlag):
-        aehnlichkeit = 0
         a = Admin()
         personid = partnervorschlag.get_person_id()
         partnerid = partnervorschlag.get_partner_id()
@@ -48,24 +20,24 @@ class Algorithmus:
         lernvorliebepartner = a.get_lernvorliebe_by_id(partnerprofil.get_lernvorlieben_id())
         lernvorliebenpe = [lernvorliebeperson.get_frequenz(), lernvorliebeperson.get_extrovertiertheit(), lernvorliebeperson.get_remote_praesenz()]
         lernvorliebenpa = [lernvorliebepartner.get_frequenz(), lernvorliebepartner.get_extrovertiertheit(), lernvorliebepartner.get_remote_praesenz()]
+        aehnlichkeit = 0
         for x in lernvorliebenpe:
             count = 0
             if x - lernvorliebenpa[count] == 0:
-                aehnlichkeit += 10
+                aehnlichkeit += 10 + lernvorliebenpa[count] + lernvorliebenpe[count]
                 count += 1
             elif x - lernvorliebenpa[count] == 1 or -1:
-                aehnlichkeit += 7
+                aehnlichkeit += 5 + lernvorliebenpa[count] + lernvorliebenpe[count]
                 count += 1
             elif x - lernvorliebenpa[count] == 2 or -2:
-                aehnlichkeit += 4
+                aehnlichkeit += 1 + lernvorliebenpa[count] + lernvorliebenpe[count]
                 count += 1
             elif x - lernvorliebenpa[count] == 3 or -3:
-                aehnlichkeit += 2
+                aehnlichkeit -= 2 - lernvorliebenpa[count] - lernvorliebenpe[count]
                 count += 1
             else:
-                aehnlichkeit += 0
+                aehnlichkeit -= 5 - lernvorliebenpa[count] - lernvorliebenpe[count]
                 count += 1
-
         with PartnerVorschlagMapper() as mapper:
             result = mapper.find_by_key(partnervorschlag.get_id())
             result.set_aehnlichkeit(aehnlichkeit)
@@ -74,7 +46,7 @@ class Algorithmus:
 
 adm = Admin()
 algo = Algorithmus()
-print(algo.match(adm.get_partner_vorschlag_by_id(1)))
+print(algo.match(adm.get_partner_vorschlag_by_id(5)))
 
 
 
