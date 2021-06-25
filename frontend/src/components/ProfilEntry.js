8db004506d902f454f4c4f0e0c0d063027b7b22e
Moritz <mm295@hdm-stuttgart.de>
2021-06-25 15:36:01 +0200

    Cleanup: Ungenutzte Components auskommentiert für späteres Löschen

--- frontend/src/components/ProfilEntry.js
+++ frontend/src/components/ProfilEntry.js
@@ -1,67 +1,90 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, Typography, Accordion, AccordionSummary, AccordionDetails, Grid } from '@material-ui/core';
-import { Button, ButtonGroup } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import LernvorliebenList from "./LernvorliebenList";
+{/*
+
+import
+    React, {Component}
+    from
+    'react';
+import
+    PropTypes
+    from
+    'prop-types';
+import
+    {
+        withStyles, Typography, Accordion, AccordionSummary, AccordionDetails, Grid
+    }
+    from
+    '@material-ui/core';
+import
+    {
+        Button, ButtonGroup
+    }
+    from
+    '@material-ui/core';
+import
+    ExpandMoreIcon
+    from
+    '@material-ui/icons/ExpandMore';
+import
+    LernvorliebenList
+    from
+    "./LernvorliebenList";
 
 
+    class ProfilEntry extends Component {
+        constructor(props) {
+            super(props);
 
-
-class ProfilEntry extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            profil: props.profil
+            this.state = {
+                profil: props.profil
+            }
         }
-    }
 
-    render() {
-        const { classes } = this.props;
-        const { profil } = this.state;
+        render() {
+            const {classes} = this.props;
+            const {profil} = this.state;
 
-        console.log("Aus ProfilEntry das aktuelle Lernvorliebe-Objekt:")
-        console.log(this.state);
+            console.log("Aus ProfilEntry das aktuelle Lernvorliebe-Objekt:")
+            console.log(this.state);
 
-        return (
-            <div>
-                <Grid>
-                    <Grid item>
-                        <Typography className={classes.heading}>
-                            Profil ID:
-                            {
-                                profil.getID()
-                            }
-                            }
-                            Lernvorliebe ID:
-                            {
-                                profil.getLernvorliebeID()
-                            }
-
-                        </Typography>
+            return (
+                <div>
+                    <Grid>
+                        <Grid item>
+                            <Typography className={classes.heading}>
+                                Profil ID:
+                                {
+                                    profil.getID()
+                                }
+                                }
+                                Lernvorliebe ID:
+                                {
+                                    profil.getLernvorliebeID()
+                                }
+                            </Typography>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </div>
-        )
+                </div>
+            )
+        }
+
     }
 
-}
+    /** Component specific styles
+    const styles = theme => ({
+        root: {
+            width: '100%',
+        }
+    });
 
-/** Component specific styles */
-const styles = theme => ({
-  root: {
-    width: '100%',
-  }
-});
+    /** PropTypes
+    ProfilEntry.propTypes = {
+        /** @ignore
+        //classes: PropTypes.object.isRequired,
+        /** The CustomerBO to be rendered
+        //nachricht: PropTypes.object.isRequired,
 
-/** PropTypes */
-ProfilEntry.propTypes = {
-  /** @ignore */
-  //classes: PropTypes.object.isRequired,
-  /** The CustomerBO to be rendered */
-  //nachricht: PropTypes.object.isRequired,
+    }
 
-}
-
-export default withStyles(styles)(ProfilEntry);
+export default
+    withStyles(styles)(ProfilEntry);
+*/}
